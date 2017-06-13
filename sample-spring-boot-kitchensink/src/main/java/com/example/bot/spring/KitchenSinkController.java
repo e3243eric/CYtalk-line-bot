4b5a22a4a7b128693875e93eb8926207102d9df0
e3243eric <e3243eric@cycu.org.tw>
2017-06-14 01:07:45 +0800

    新增搜尋範例

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -311,7 +311,7 @@ public class KitchenSinkController {
                 break;
             }
             case "幫我找5/11通識": {
-                ButtonsTemplate buttonsTemplate = new ButtonsTemplate(
+                ButtonsTemplate searchEX = new ButtonsTemplate(
                 		"http://ann.cycu.edu.tw/aa/ImgFile/news.aa_ann_img/36880_1_0.jpg",
                         "【通識活動】5/11(四)犀牛劇團《方舟計畫零零參》",
                         "",
@@ -320,7 +320,7 @@ public class KitchenSinkController {
                                               "http://ann.cycu.edu.tw/aa/frontend/AnnItem.jsp?sn=36880")
                                 
                         ));
-                TemplateMessage templateMessage = new TemplateMessage("Button alt text", buttonsTemplate);
+                TemplateMessage templateMessage = new TemplateMessage("Button alt text", searchEX);
                 this.reply(replyToken, templateMessage);
                 break;
             }
