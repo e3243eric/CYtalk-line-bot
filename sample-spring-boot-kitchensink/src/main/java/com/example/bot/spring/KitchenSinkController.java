e8fd1d3c19898271cd5f329d8c9df767c61c3532
e3243eric <e3243eric@cycu.org.tw>
2017-06-14 01:17:12 +0800

    通識範例

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -310,6 +310,7 @@ public class KitchenSinkController {
                 this.reply(replyToken, templateMessage);
                 break;
             }
+            /*
             case "幫我找5/11通識": {
                 ButtonsTemplate searchEX = new ButtonsTemplate(
                 		"http://ann.cycu.edu.tw/aa/ImgFile/news.aa_ann_img/36880_1_0.jpg",
@@ -324,6 +325,7 @@ public class KitchenSinkController {
                 this.reply(replyToken, templateMessage);
                 break;
             }
+            */
             case "carousel": {
                 String imageUrl = createUri("/static/buttons/1040.jpg");
                 CarouselTemplate carouselTemplate = new CarouselTemplate(
@@ -380,6 +382,7 @@ public class KitchenSinkController {
                 ));
                 break;
             default:
+            	
             	/*
                 log.info("Returns echo message {}: {}", replyToken, text);
                 this.replyText(
@@ -387,6 +390,21 @@ public class KitchenSinkController {
                         text
                 );
                 */
+            	
+            	//5/11通識.
+            	ButtonsTemplate searchEX = new ButtonsTemplate(
+                		"http://ann.cycu.edu.tw/aa/ImgFile/news.aa_ann_img/36880_1_0.jpg",
+                        "【通識活動】5/11(四)犀牛劇團《方舟計畫零零參》",
+                        "",
+                        Arrays.asList(
+                                new URIAction("公告網頁",
+                                              "http://ann.cycu.edu.tw/aa/frontend/AnnItem.jsp?sn=36880")
+                                
+                        ));
+                TemplateMessage templateMessage = new TemplateMessage("Button alt text", searchEX);
+                this.reply(replyToken, templateMessage);
+                //******
+                
                 break;
         }
     }
