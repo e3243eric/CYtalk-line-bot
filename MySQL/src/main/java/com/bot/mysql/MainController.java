ca5fbb54b81542b7461df037152a35e4a16ca984
e3243eric <e3243eric@cycu.org.tw>
2017-08-15 03:09:02 +0800

    8/15 for MySQL

--- MySQL/src/main/java/com/bot/mysql/MainController.java
+++ MySQL/src/main/java/com/bot/mysql/MainController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import hello.User;
-import hello.UserRepository;
+import com.bot.mysql.User;
+import com.bot.mysql.UserRepository;
 
 @Controller    // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
--- MySQL/src/main/java/com/bot/mysql/UserRepository.java
+++ MySQL/src/main/java/com/bot/mysql/UserRepository.java
@@ -2,7 +2,7 @@ package com.bot.mysql;
 
 import org.springframework.data.repository.CrudRepository;
 
-import hello.User;
+import com.bot.mysql.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
